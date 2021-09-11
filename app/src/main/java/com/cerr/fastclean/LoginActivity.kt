3d6361700abd115fef0f48f96c662d0f45bcd7dd
Esteban Andaluz <esteban.andaluz@epn.edu.ec>
2021-09-11 03:23:02 -0500

    Autenticación con firebase

--- app/src/main/java/com/cerr/fastclean/LoginActivity.kt
+++ app/src/main/java/com/cerr/fastclean/LoginActivity.kt
@@ -5,20 +5,28 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
     lateinit var buttonLogin: Button
-    lateinit var textViewRegistro: TextView
+    lateinit var editTextUsuario: EditText
+    lateinit var editTextPassword: EditText
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        buttonLogin = findViewById(R.id.buttonLogin)
         setupActivityLink()
+        buttonLogin = findViewById(R.id.buttonLogin)
+        editTextUsuario = findViewById(R.id.editTextUsuario)
+        editTextPassword = findViewById(R.id.editTextPassword)
+        auth = Firebase.auth
         buttonLogin.setOnClickListener{
-            val intencion = Intent(this, PrincipalActivity::class.java)
-            startActivity(intencion)
-
+           AutenticarUsuario(editTextUsuario.text.toString(),editTextPassword.text.toString())
         }
     }
     fun setupActivityLink() {
@@ -29,4 +37,18 @@ class LoginActivity : AppCompatActivity() {
             startActivity(switchActivityIntent)
         }
     }
+    fun AutenticarUsuario(email:String, password:String){
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    var intent = Intent(this,PrincipalActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    Toast.makeText(baseContext, task.exception!!.message,
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
 }
