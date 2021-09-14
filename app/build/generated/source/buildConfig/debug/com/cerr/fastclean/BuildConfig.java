2a9b7f3fa476f9d50ab79659a2e0d7c80a102813
Rodman <rodman.iniguez@epn.edu.ec>
2021-09-14 10:07:19 -0500

    conexión a base / creación CardView

---
+++ app/build/generated/source/buildConfig/debug/com/cerr/fastclean/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.cerr.fastclean;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.cerr.fastclean";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
