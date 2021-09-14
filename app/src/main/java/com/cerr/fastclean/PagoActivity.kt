b088a4db6f0170c549e4ee50985b3f4a206b5e99
Ricardo Paredes <andres.paredes01@epn.edu.ec>
2021-09-14 15:47:29 -0500

    Adición de pantalla de pago y tipo de pago

---
+++ app/src/main/java/com/cerr/fastclean/PagoActivity.kt
@@ -0,0 +1,12 @@
+package com.cerr.fastclean
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PagoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pago)
+    }
+
+}
---
+++ app/src/main/java/com/cerr/fastclean/TipoPagoActivity.kt
@@ -0,0 +1,11 @@
+package com.cerr.fastclean
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TipoPagoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tipo_pago)
+    }
+}
